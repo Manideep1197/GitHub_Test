567d5b8633cff2e8e9b09e53456e52ff3fa353df
Cherukumanideep <56860361+Cherukumanideep@users.noreply.github.com>
2020-06-08 20:10:54 +0530

    Add project files.

---
+++ production/Tables/brands.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [production].[brands] (
+    [brand_id]   INT           IDENTITY (1, 1) NOT NULL,
+    [brand_name] VARCHAR (255) NOT NULL,
+    [brand_type] VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([brand_id] ASC)
+);
+
